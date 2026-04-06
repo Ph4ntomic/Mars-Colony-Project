8d870a9f2edb8683b41b4810c70fe74b62cfa16c
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-04-06 19:44:09 +0200

    chore(skeleton): format code

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -1,22 +1,22 @@
 export const GraphSkeleton = () => (
-  <div 
+  <div
     style={{ backgroundColor: 'var(--secondary)' }}
     className="animate-pulse rounded-lg w-full h-[300px] flex items-end p-4 gap-2"
   >
-    <div 
-      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }} 
+    <div
+      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }}
       className="w-1/4 h-1/2 rounded"
     ></div>
-    <div 
-      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }} 
+    <div
+      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }}
       className="w-1/4 h-3/4 rounded"
     ></div>
-    <div 
-      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }} 
+    <div
+      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }}
       className="w-1/4 h-2/3 rounded"
     ></div>
-    <div 
-      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }} 
+    <div
+      style={{ backgroundColor: 'var(--bg)', opacity: 0.7 }}
       className="w-1/4 h-full rounded"
     ></div>
   </div>
