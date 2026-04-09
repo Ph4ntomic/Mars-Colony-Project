b8f526d22ee4de977c16f84cbf3167f5509f8e19
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-04-09 14:51:41 +0200

    feat(sql): add query to get all citizens by name

---
+++ sql/getAllCitizensByName.sql
@@ -0,0 +1,17 @@
+SELECT
+    b.bewohner_id,
+    b.vorname,
+    b.nachname,
+    CONCAT(CONCAT(CONCAT(CONCAT(a.stra_e, ' '), a.hausnummer), ', '), s.stadt_name) AS adresse,
+    CASE 
+        WHEN m.bewohner_id IS NOT NULL THEN 'Ja' 
+        ELSE 'Nein' 
+    END AS ist_mitarbeiter
+FROM BEWOHNER b
+LEFT JOIN MITARBEITER m 
+    ON b.bewohner_id = m.bewohner_id
+INNER JOIN ADRESSE a 
+    ON b.adresse_id = a.adresse_id
+INNER JOIN STADT s 
+    ON a.stadt_id = s.stadt_id
+WHERE CONCAT(b.vorname, ' ', b.nachname) LIKE CONCAT('%', ?, '%');
